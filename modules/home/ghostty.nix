834fd4b65f7409fed10f7694e40c8f604af32db6
playfairs <root@playfairs.cc>
2025-12-24 01:09:09 -0600

    update: ghostty font size

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -15,7 +15,7 @@
       macos-titlebar-style = "hidden";
       quit-after-last-window-closed = true;
       window-save-state = "never";
-      font-size = 13;
+      font-size = 11;
       cursor-style = "bar";
     };
   };
